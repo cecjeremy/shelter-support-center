a48741f3e13cca1e272d100cee28afa8bb98d335
Jon Spaeth <jon.spaeth@voicefoundry.com>
2022-02-18 15:35:50 -0600

    updating config with connect instance ids from each environment

--- config.ts
+++ config.ts
@@ -45,21 +45,21 @@ export const config: Configuration = {
       profile: 'shelter-dev',
       region: 'us-east-1',
       id: '564741965342',
-      connectInstanceId: ''
+      connectInstanceId: '474a2e52-39e2-46e5-975d-2b3b17f01251'
     },
     test: {
       profile: 'shelter-test',
       region: 'us-east-1',
       id: '824188805183',
       approval: true,
-      connectInstanceId: ''
+      connectInstanceId: '03cd8add-adbf-4a25-938f-4fc3a508566d'
     },
     prod: {
       profile: 'shelter-prod',
       region: 'us-east-1',
       id: '110716305825',
       approval: true,
-      connectInstanceId: ''
+      connectInstanceId: '373f8a4f-a7fe-48ad-977d-b2e0c55b9e8f'
     }
   },
   packages: {
