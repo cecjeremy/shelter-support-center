24a54d2f643e732680d768086774b967d32daffa
jon.spaeth <jon.spaeth@voicefoundry.com>
2022-02-24 15:42:46 -0600

    adding default value to response - 1  minute

--- lib/lambdas/secondsToMinutesConverter/SecondsToMinutesConverter.ts
+++ lib/lambdas/secondsToMinutesConverter/SecondsToMinutesConverter.ts
@@ -6,10 +6,16 @@ export class SecondsToMinutesConverter {
   public handler(event: ConnectContactFlowEvent) {
     logger.info(event);
 
-    const seconds = event.Details.Parameters.seconds;
+    // if the oldest contact in the queue doesn't exist the
+    // input seconds could potentially be null/undefined
+    // so this ternary will default to always reporting at
+    // least 1 minute of wait time.
+    const seconds = event.Details.Parameters.seconds ?? 1;
 
     try {
-      return { minutes: Math.ceil(parseInt(seconds) / 60) };
+      const result = { minutes: Math.ceil(parseInt(seconds) / 60) };
+      logger.info(result);
+      return result;
     } catch (err) {
       logger.error({ err });
       throw err;
