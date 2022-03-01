2b9fd30a1fb1e7ec375d7a274bbccdcbba5fb24e
jon.spaeth <jon.spaeth@voicefoundry.com>
2022-03-01 09:22:11 -0600

    fixing callback

--- lib/lambdas/secondsToMinutesConverter/SecondsToMinutesConverter.ts
+++ lib/lambdas/secondsToMinutesConverter/SecondsToMinutesConverter.ts
@@ -3,14 +3,8 @@ import { Callback, ConnectContactFlowEvent, Context } from 'aws-lambda';
 import logger from '../../services/logger';
 
 export class SecondsToMinutesConverter {
-  public handler(event: ConnectContactFlowEvent, ctx: Context, cb: Callback) {
+  public handler(event: ConnectContactFlowEvent, _ctx: Context, cb: Callback) {
     logger.info(event);
-
-    // if the oldest contact in the queue doesn't exist the
-    // input seconds could potentially be null/undefined
-    // so this ternary will default to always reporting at
-    // least 1 minute of wait time.
-
     try {
       let seconds = parseInt(event.Details.Parameters.seconds);
       seconds = isNaN(seconds) || seconds === 0 ? 1 : seconds;
--- lib/lambdas/secondsToMinutesConverter/handler.ts
+++ lib/lambdas/secondsToMinutesConverter/handler.ts
@@ -1,9 +1,13 @@
-import { ConnectContactFlowEvent } from 'aws-lambda';
+import { Callback, ConnectContactFlowEvent, ConnectContactFlowHandler, Context } from 'aws-lambda';
 
 import { SecondsToMinutesConverter } from './SecondsToMinutesConverter';
 
 const lambda = new SecondsToMinutesConverter();
 
-export const handler = (event: ConnectContactFlowEvent) => {
-  return lambda.handler(event);
+export const handler: ConnectContactFlowHandler = (
+  event: ConnectContactFlowEvent,
+  context: Context,
+  callback: Callback
+) => {
+  return lambda.handler(event, context, callback);
 };
