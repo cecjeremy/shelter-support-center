14d7181ea4d5a007810b6b3894b9fa1326a7e5c0
jon.spaeth <jon.spaeth@voicefoundry.com>
2022-04-14 12:57:30 -0500

    updating migration scripts

--- migration/migrate.ts
+++ migration/migrate.ts
@@ -1,57 +1,13 @@
 #!ts-node
-import {
-  CLI,
-  CLIBuilder,
-  DefaultNameTransformer,
-  DeployableNameProps,
-  DeployEnvironment,
-  INameTransformer,
-  StorableNameProps
-} from '@ttec-dig-vf/vf-deploy';
-
-export class ApplicationNameTransformer extends DefaultNameTransformer {
-  public getStorableName = <T extends StorableNameProps>(props: T): string => {
-    if (props.transformType !== 'Lambda') {
-      return props.deployedName;
-    }
-
-    if (props.deployedName.includes('getConfig')) {
-      return 'getConfig-shelter-connect-admin';
-    }
-    if (props.deployedName.includes('openCheck')) {
-      return 'openCheck-shelter-connect-admin';
-    }
-    if (props.deployedName.includes('secondsToMinutesConverter')) {
-      return 'secondsToMinutesConverter-shelter-support-center';
-    }
-
-    return props.deployedName;
-  };
-
-  public getDeployableName = <T extends DeployableNameProps>(props: T): string => {
-    if (props.transformType !== 'Lambda') {
-      return props.storableName;
-    }
-
-    switch (props.deployEnvironment) {
-      case DeployEnvironment.Development:
-        return `${props.storableName}-dev`;
-      case DeployEnvironment.Test:
-        return `${props.storableName}-test`;
-      case DeployEnvironment.Production:
-        return `${props.storableName}-prod`;
-      default:
-        return props.storableName;
-    }
-  };
-}
+import { CLI, CLIBuilder, EnvironmentNameTransformer, INameTransformer } from '@ttec-dig-vf/vf-deploy';
+import { config } from './config';
 
 // Builder class to support customization.
 export class Builder implements CLIBuilder {
   getNameTransformer(): INameTransformer {
-    return new ApplicationNameTransformer();
+    return new EnvironmentNameTransformer();
   }
 }
 
 // Executes the actual command-line interface for the deploy manager framework.
-CLI.exec(new Builder());
+new CLI(new Builder(), config).exec();
