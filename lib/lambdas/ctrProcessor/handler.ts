51faa51a6d6c4b3cdebd16cc788616df1cb57a16
jwright <jacob.wright@voicefoundry.com>
2023-03-09 13:50:21 -0700

    update to send records in single file like it was read

--- lib/lambdas/ctrProcessor/handler.ts
+++ lib/lambdas/ctrProcessor/handler.ts
@@ -53,56 +53,58 @@ export const handler = async (event: S3CreateEvent): Promise<void> => {
   const cdkStackPrefix = process.env.CDK_STACK_PREFIX;
   logger.info(`Found ${event.Records.length} records to process`);
   for (const record of event.Records) {
-    try {
-      const bucketName = record.s3.bucket.name;
-      const objectKey = record.s3.object.key;
-      const objectName = getObjectNameFromKey(record.s3.object.key);
-      logger.info(`Processing object: ${objectName}`);
+    const bucketName = record.s3.bucket.name;
+    const objectKey = record.s3.object.key;
+    const objectName = getObjectNameFromKey(record.s3.object.key);
+    logger.info(`Processing object: ${objectName}`);
 
-      // getObject from S3 and convert to string
-      const object = await s3.getObject(bucketName, objectKey); //each s3 file
-      let newObject = '';
-      const contents = await readableToString(object.Body);
-      logger.info(`Contents: ${contents}`);
+    // getObject from S3 and convert to string
+    const object = await s3.getObject(bucketName, objectKey); //each s3 file
+    let newObject = '';
+    const contents = await readableToString(object.Body);
+    logger.info(`Contents: ${contents}`);
 
-      const lines = contents.split('\n');
-      logger.info(`Found ${lines.length} lines in file`);
+    const lines = contents.split('\n');
+    logger.info(`Found ${lines.length} lines in file`);
 
-      for (const [index, line] of lines.entries()) {
-        if (line.trim().length === 0) {
-          // sometimes there is a trailing empty line at end of the file, ignore it
-          continue;
-        }
-        logger.info(`Processing file line: ${index}`);
-        logger.info(line);
-
-        let ctr = JSON.parse(line) as IContactTraceRecord;
-        if (!ctr.Agent.ConnectedToAgentTimestamp || ctr.Recordings.length > 0) {
-          ctr = updateRecordingLocation(
-            ctr,
-            `connect/${cdkStackPrefix}/${cdkStackPrefix}-recordings`,
-            'Analysis/Voice/Redacted'
-          );
-          const wavFileName = ctr.Recording.Location.split('/')[1];
-
-          ctr = updateRecordingLocation(ctr, 'connect', 'Analysis/Voice/Redacted');
-          ctr = updateRecordingLocation(
-            ctr,
-            wavFileName,
-            `${ctr.ContactId}_call_recording_redacted${ctr.Agent.ConnectedToAgentTimestamp}.wav`
-          );
-
-          logger.info(JSON.stringify(ctr));
-        }
-        newObject += JSON.stringify(ctr) + '\n';
+    for (const [index, line] of lines.entries()) {
+      if (line.trim().length === 0) {
+        // sometimes there is a trailing empty line at end of the file, ignore it
+        continue;
       }
-      const newObjectKey = objectKey.replace('/original', '');
-      logger.info(newObjectKey);
-      logger.info(newObject);
-      await s3.putObject(bucketName, newObjectKey, newObject);
-    } catch (e) {
-      logger.error(e);
-      logger.error(`Unable to process file: ${record.s3.object.key}`);
+      logger.info(`Processing file line: ${index}`);
+      logger.info(line);
+
+      let ctr = JSON.parse(line) as IContactTraceRecord;
+      if (ctr.Agent.ConnectedToAgentTimestamp) {
+        ctr = updateRecordingLocation(
+          ctr,
+          `connect/${cdkStackPrefix}/${cdkStackPrefix}-recordings`,
+          'Analysis/Voice/Redacted'
+        );
+        const wavFileName = ctr.Recording.Location.split('/')[1];
+
+        ctr = updateRecordingLocation(ctr, 'connect', 'Analysis/Voice/Redacted');
+        ctr = updateRecordingLocation(
+          ctr,
+          wavFileName,
+          `${ctr.ContactId}_call_recording_redacted${ctr.Agent.ConnectedToAgentTimestamp}.wav`
+        );
+
+        logger.info(JSON.stringify(ctr));
+      }
+      newObject += JSON.stringify(ctr) + '\n';
     }
+    const newObjectKey = objectKey.replace('/original', '');
+    logger.info(newObjectKey);
+    logger.info(newObject);
+    const response = await s3.putObject(bucketName, newObjectKey, newObject);
+    if (response.$metadata.httpStatusCode !== 200) {
+      logger.error(response.$metadata.httpStatusCode);
+      logger.error(`Unable to process file: ${record.s3.object.key}`);
+      logger.error(response);
+    }
+    logger.info(`Processed file: ${record.s3.object.key}`);
   }
+  logger.info('Completed processing');
 };
