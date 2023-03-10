63d43d75042d3feca0d7ff144d0991158c1136ea
jwright <jacob.wright@voicefoundry.com>
2023-03-10 13:31:12 -0700

    update key permissions

--- lib/stacks/CalabrioQmStack.ts
+++ lib/stacks/CalabrioQmStack.ts
@@ -132,6 +132,7 @@ export class CalabrioQmStack extends Stack {
         filters: [{ prefix: 'original/ctr/' }]
       })
     );
+    props.encryptionKey.grantEncryptDecrypt(ctrProcessorLambda);
 
     this.qmBucket.grantReadWrite(ctrProcessorLambda);
   }
